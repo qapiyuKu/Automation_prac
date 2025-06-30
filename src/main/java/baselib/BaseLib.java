3c1eac301c1ef9a3e8f0e53c3a87f0883f8316c3
Piyush Kumar <qa.piyushkumar@gmail.com>
2025-07-01 00:31:57 +0530

    baselibrarycreated

--- src/main/java/baselib/BaseLib.java
+++ src/main/java/baselib/BaseLib.java
@@ -1,4 +1,39 @@
 package baselib;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import utilites.ReadPropertyFile;
+
+import java.io.IOException;
+
 public class BaseLib {
+    public static WebDriver driver;
+     static String BrowserName;
+     static String  URL;
+
+
+    public void setup() throws IOException {
+    BrowserName=ReadPropertyFile.getPropertyvalue("browser");
+    URL=ReadPropertyFile.getPropertyvalue("url");
+   if(BrowserName.equalsIgnoreCase("chrome"))
+   {
+       driver=new ChromeDriver();
+       driver.get(URL);
+   }
+
+    else if(BrowserName.equalsIgnoreCase("edge"))
+    {
+        driver=new EdgeDriver();
+        driver.get(URL);
+    }
+}
+
+    public static void main(String[] args) throws IOException {
+        BaseLib b=new BaseLib();
+        b.setup();
+
+    }
+
 }
--- src/main/java/utilites/ReadPropertyFile.java
+++ src/main/java/utilites/ReadPropertyFile.java
@@ -7,12 +7,22 @@ import java.util.Properties;
 
 public class ReadPropertyFile {
 
-    public static void main(String[] args) throws IOException {
+
+
+    public static String getPropertyvalue(String s) throws IOException {
         String path="C:\\Users\\Nitin\\IdeaProjects\\Automation_prac\\src\\main\\resources\\config.properties";
         FileInputStream fis=new FileInputStream(path);
         Properties prop=new Properties();
         prop.load(fis);
-        String br = prop.getProperty("browser");
+        String br = prop.getProperty(s);
         System.out.println(br);
+        return br;
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+        ReadPropertyFile.getPropertyvalue("browser");
+
     }
 }
